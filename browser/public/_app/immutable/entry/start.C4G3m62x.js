0e51c6449c8723feab52a566ca43eb19759d86f3
San <sganis@gmail.com>
2025-09-17 17:15:07 +0300

    Added browser build

---
+++ browser/public/_app/immutable/entry/start.C4G3m62x.js
@@ -0,0 +1,2 @@
+import{l as o,a as r}from"../chunks/D22zCRQs.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.C4G3m62x.js.map
--- browser/public/_app/immutable/entry/start.Cxb1Fj1d.js
+++
@@ -1,2 +0,0 @@
-import{l as o,a as r}from"../chunks/3monJsr1.js";export{o as load_css,r as start};
-//# sourceMappingURL=start.Cxb1Fj1d.js.map
